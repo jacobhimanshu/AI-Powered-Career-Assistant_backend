61d62524731da35bcbc1affca29440f748ba616a
Himanshu Maurya <himanshumaurya@5854>
2026-05-11 00:52:10 +0530

    updated project

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 connectToDB()
 
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
 app.get("/", (req, res) => {
   res.send("hi");
--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ dotenv.config({ path: "./.env" });
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
 }))
 
